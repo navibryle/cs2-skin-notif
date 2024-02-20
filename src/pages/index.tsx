fc7445fe599439ce9a24f795cf72f0bb6faa24f5
navibryle <naviivan321@gmail.com>
2024-02-20 16:24:07 -0700

    cleanup index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+
+export default function Home() {
+
+  return (
+    <>
+      <Head>
+        <title>Cs market notifier</title>
+        <meta name="description" content="Notifies users with their selected counter strike market trend" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
+
