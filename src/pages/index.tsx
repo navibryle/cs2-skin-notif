658d8d838152e94c808870e8a73b323572a158b3
navibryle <naviivan321@gmail.com>
2024-02-20 19:23:56 -0700

    added search bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from "next/head";
-
+import TextField from "@mui/material/TextField"
+import { IconButton } from "@mui/material";
+import ZoomInIcon from '@mui/icons-material/ZoomIn';
 
 export default function Home() {
-
   return (
     <>
       <Head>
@@ -10,6 +11,16 @@ export default function Home() {
         <meta name="description" content="Notifies users with their selected counter strike market trend" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="flex h-screen">
+        <div className="text-center m-auto">
+          <span className="flex">
+            <TextField id="outlined-basic" variant="outlined" label="Search"/>
+            <IconButton aria-label = "search" className="align-middle">
+              <ZoomInIcon/>
+            </IconButton>
+          </span>
+        </div>
+      </div>
     </>
   );
 }
