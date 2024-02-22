3371e08e7fedd112d2edfc50520e24a92d04e991
navibryle <naviivan321@gmail.com>
2024-02-21 18:24:22 -0700

    simple animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,13 @@ import Head from "next/head";
 import TextField from "@mui/material/TextField"
 import { IconButton } from "@mui/material";
 import ZoomInIcon from '@mui/icons-material/ZoomIn';
+import { useState } from "react";
 
 export default function Home() {
+  const [searchBarMargin,setSearchBarMargin] = useState({marginTop:"50vh"});
+  const searchBtnClick = () => {
+    setSearchBarMargin({marginTop:"10vh"});
+  }
   return (
     <>
       <Head>
@@ -11,11 +16,11 @@ export default function Home() {
         <meta name="description" content="Notifies users with their selected counter strike market trend" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex h-screen">
-        <div className="text-center m-auto">
-          <span className="flex">
+      <div id="searchBar" className="flex flex-col" style={searchBarMargin}>
+        <div className="text-center">
+          <span className="flex justify-center">
             <TextField id="outlined-basic" variant="outlined" label="Search"/>
-            <IconButton aria-label = "search" className="align-middle">
+            <IconButton aria-label = "search" className="align-middle" onClick={searchBtnClick}>
               <ZoomInIcon/>
             </IconButton>
           </span>
