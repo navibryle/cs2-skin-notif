4d86d15574cdc6548b10e1ba088dc9c1618bd6c2
navibryle <naviivan321@gmail.com>
2024-02-27 16:02:26 -0700

    got a working version of parser and init script

--- src/dbInstall/parser.py
+++ src/dbInstall/parser.py
@@ -11,60 +11,67 @@ if (len(sys.argv) != 3):
 gunId=0
 skinId=0
 gunsCreated = []
+skinCreated = {}
 gunIdMap = {}
 rootFolder = sys.argv[2]
 sqlFile = rootFolder+"/dataInit.sql"
 
+def findIdxOfLastDash(aString):
+    lastIdx = 0
+    for idx,i in enumerate(aString):
+        if i == "-":
+            lastIdx = idx
+    return lastIdx
+
 def parseJsonEntry(fName):
     with open(fName) as file:
-        global gunId,skinId
+        global gunId,skinId,gunsCreated,gunIdMap,rootFolder,sqlFile
         contentKey = "content"
         fileJson = json.load(file)
-        if contentKey in fileJson.keys():
-            for _,i in fileJson[contentKey].items():
-                for k in i:
-                    name = ""
-                    skinName = ""
-                    gunJson = k["name"]
-                    curPos = 0
-                    while (gunJson[curPos] != '|'):
-                        name += gunJson[curPos]
+        with open(sqlFile,"a") as f:
+            if contentKey in fileJson.keys():
+                for _,i in fileJson[contentKey].items():
+                    for k in i:
+                        name = ""
+                        skinName = ""
+                        gunJson = k["name"].split()
+                        curPos = 0
+                        while (gunJson[curPos] != "|"):
+                            name += gunJson[curPos] + " "
+                            curPos += 1
                         curPos += 1
-                    curPos += 1
-                    while (curPos < len(gunJson) and gunJson[curPos] != "-"):
-                        skinName += gunJson[curPos]
-                        curPos+=1
-                    newPath = []
-                    name = name.strip()
-                    if name not in gunsCreated: # check if we need to add gun to the table
-                        gunsCreated.append("name")
-                        with open(sqlFile,"a") as f:
+                        while (curPos < len(gunJson) and gunJson[curPos] != "-"):
+                            skinName += gunJson[curPos] + " "
+                            curPos += 1
+                        newPath = []
+                        name = name.strip().replace(" ","_")
+                        skinName = skinName.strip().replace(" ","_")
+
+                        if name not in gunsCreated: # check if we need to add gun to the table
+                            gunsCreated.append(name)
+                            gunIdMap[name] = gunId
+                            skinCreated[name] = []
                             tmp = "INSERT INTO GUNS (ID,NAME) VALUES (" +str(gunId)+",'"+name+"');\n"
                             f.write(tmp)
-                        gunIdMap[name] = gunId
-                        gunId += 1
-                    with open(sqlFile,"a") as f:
-                        tmp = "INSERT INTO SKINS (ID,NAME,GUN_ID) VALUES (" + str(skinId) +",'"+skinName+"',"+str(gunIdMap[name])+";\n"
-                        f.write(tmp)
-                        skinId += 1
-                    skinName = skinName.strip().replace(" ","_")
-                    rootPath = rootFolder+"/"+name
-                    for wearTier,z in k["wears"].items():
-                        newPath.append(rootPath+"/"+wearTier)
-                    for idx,h in enumerate(newPath):
-                        h = h.replace(" ","_")
-                        newPath[idx] = h
-                        if not os.path.exists(h):
-                            os.makedirs(h)
-                    for (idx,(wearTier,z)) in enumerate(k["wears"].items()):
-                        with open(newPath[idx]+"/"+skinName+".png","wb") as im:
-                            im.write(requests.get(z).content)
-        else:
-            print(f"DEBUGPRINT[1]: parser.py:45 (after else:)")
-            print(fName)
-            print(f"DEBUGPRINT[3]: parser.py:47 (after print(fName))")
-            print(fileJson)
-            print(f"DEBUGPRINT[2]: parser.py:48 (after print(fileJson))")
+                            gunId += 1
+                        if skinName not in skinCreated[name]: # check if we need to add skin to the table
+                            skinCreated[name].append(skinName)
+                            tmp = "INSERT INTO SKINS (ID,NAME,GUN_ID) VALUES (" + str(skinId) +",'"+skinName+"',"+str(gunIdMap[name])+");\n"
+                            f.write(tmp)
+                            skinId += 1
+                        rootPath = rootFolder+"/"+name
+                        for wearTier,z in k["wears"].items():
+                            newPath.append(rootPath+"/"+wearTier)
+                        for idx,h in enumerate(newPath):
+                            h = h.replace(" ","_")
+                            newPath[idx] = h
+                            if not os.path.exists(h):
+                                os.makedirs(h)
+                        for (idx,(wearTier,z)) in enumerate(k["wears"].items()):
+                            with open(newPath[idx]+"/"+skinName+".png","wb") as im:
+                                im.write(requests.get(z).content)
+if (not os.path.exists(rootFolder)):
+    os.makedirs(rootFolder)
 if (not os.path.exists(sqlFile)):
     with open(sqlFile,"w") as f:
         print("created init data scripts file")
