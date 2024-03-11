efa0194727897bfca5cfdc7966569cf63189e8e5
navibryle <naviivan321@gmail.com>
2024-03-10 18:35:35 -0600

    login form done

---
+++ src/pages/login/[user].tsx
@@ -0,0 +1,15 @@
+import { Button, TextField } from "@mui/material";
+
+export default function Page(){
+    return (
+        <div className="flex w-full justify-center h-screen">
+            <div className="flex flex-col w-2/5 justify-center">
+                <TextField variant="standard" label="Username"/>
+                <TextField variant="standard" label="Password" className="mt-2"/>
+                <div className="flex mt-2 justify-end">
+                    <Button variant="contained" color="success" className="bg-sky-700">Login</Button>
+                </div>
+            </div>
+        </div>
+    );
+}
